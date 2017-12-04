df0bca8e7864736c272e24e27d9a5adf3abdbc37
bengreenier <begreen@microsoft.com>
2017-12-04 09:51:36 -0500

    more tests

---
+++ Plugins/UnityClientPlugin/WebRtcWrapper.UnitTests/CancellableTaskTests.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebRtcWrapper.Signalling;
+using WebRtcWrapper.Utilities;
+using System.Threading.Tasks;
+
+namespace WebRtcWrapper.UnitTests
+{
+	[TestClass]
+	public class CancellableTaskTests
+	{
+		[TestMethod]
+		public void TaskRuns()
+		{
+			var value = 0;
+			var instance = CancellableTask.Run(() =>
+			{
+				value += 1;
+			});
+
+			instance.Task.Wait();
+
+			Assert.AreEqual(1, value);
+		}
+
+		[TestMethod]
+		public void TaskCancels()
+		{
+			var value = 0;
+			var instance = CancellableTask.Run(async () =>
+			{
+				await Task.Delay(5000);
+				value += 1;
+			});
+
+			instance.Cancel();
+			instance.Task.Wait();
+
+			Assert.AreEqual(0, value);
+		}
+	}
+}
--- Plugins/UnityClientPlugin/WebRtcWrapper.UnitTests/Signalling2Tests.cs
+++ Plugins/UnityClientPlugin/WebRtcWrapper.UnitTests/Signalling2Tests.cs
@@ -22,7 +22,7 @@ namespace WebRtcWrapper.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-			this.mockHttp
+			//this.mockHttp
         }
     }
 }
